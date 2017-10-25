c7676e4a760268fe422a99b9344036977dd2cc53
Dmitry Zaitsev <hhrhhr@gmail.com>
2017-10-25 13:14:03 +0300

    update map example

--- docs/teleport.js
+++ docs/teleport.js
@@ -1,44 +1,53 @@
-function add_markers(){
-L.marker([-35856.3671875,-53979.21484375],{title:"MT_Teleporter_ABE_Barrage"}).addTo(Teleport);
-L.marker([-59009.046875,-157353.875],{title:"MT_Teleporter_ABE_CentralAbessa"}).addTo(Teleport);
-L.marker([-30659.837890625,-133868.84375],{title:"MT_Teleporter_ABE_ConverterNorth"}).addTo(Teleport);
-L.marker([-60699.18359375,-84866.6796875],{title:"MT_Teleporter_ABE_ConverterSouth"}).addTo(Teleport);
-L.marker([-83969.921875,-179900.265625],{title:"MT_Teleporter_ABE_Relict"}).addTo(Teleport);
-L.marker([-102913.6484375,-200509.640625],{title:"MT_Teleporter_ABE_SmallFarm"}).addTo(Teleport);
-L.marker([11518.771484375,-62752.546875],{title:"MT_Teleporter_ABE_Zardom"}).addTo(Teleport);
-L.marker([-307594.625,-183427.71875],{title:"MT_Teleporter_EDA_BSKIsland"}).addTo(Teleport);
-L.marker([-241486.71875,-231641.171875],{title:"MT_Teleporter_EDA_CompanyPlace"}).addTo(Teleport);
-L.marker([-182784.9375,-163178.4375],{title:"MT_Teleporter_EDA_Converter"}).addTo(Teleport);
-L.marker([-206697.859375,-156440.546875],{title:"MT_Teleporter_EDA_FirstBase"}).addTo(Teleport);
-L.marker([-253157.15625,-206159.28125],{title:"MT_Teleporter_EDA_ForbiddenValley"}).addTo(Teleport);
-L.marker([-217958.71875,-133608.15625],{title:"MT_Teleporter_EDA_Goliet"}).addTo(Teleport);
-L.marker([-221806.96875,-107719.4765625],{title:"MT_Teleporter_EDA_GolietNorth"}).addTo(Teleport);
-L.marker([-238221.640625,-114016.8046875],{title:"MT_Teleporter_EDA_HotelGoliet"}).addTo(Teleport);
-L.marker([-215739.53125,-58653.9765625],{title:"MT_Teleporter_EDA_MountainLake"}).addTo(Teleport);
-L.marker([-278752,-222325.390625],{title:"MT_Teleporter_EDA_PilgerLocation"}).addTo(Teleport);
-L.marker([-270559.78125,-131398.28125],{title:"MT_Teleporter_EDA_RiverDelta"}).addTo(Teleport);
-L.marker([-235846.578125,-173422.21875],{title:"MT_Teleporter_EDA_WoodElevator"}).addTo(Teleport);
-L.marker([-101783.5625,-129163.6640625],{title:"MT_Teleporter_HOM_HomeBase"}).addTo(Teleport);
-L.marker([19380.4296875,146861.25],{title:"MT_Teleporter_IGN_Cathedral"}).addTo(Teleport);
-L.marker([-54469.66015625,77148.9140625],{title:"MT_Teleporter_IGN_ClericCamp"}).addTo(Teleport);
-L.marker([49884.8359375,-5830.5307617188],{title:"MT_Teleporter_IGN_Converter"}).addTo(Teleport);
-L.marker([-29649.6796875,95234.2890625],{title:"MT_Teleporter_IGN_Crater"}).addTo(Teleport);
-L.marker([52558.296875,84652.5390625],{title:"MT_Teleporter_IGN_Hangar"}).addTo(Teleport);
-L.marker([28492.44140625,116783.1328125],{title:"MT_Teleporter_IGN_Hort"}).addTo(Teleport);
-L.marker([-85513.8828125,112025.953125],{title:"MT_Teleporter_IGN_LavaLake"}).addTo(Teleport);
-L.marker([-11586.33203125,52029.08984375],{title:"MT_Teleporter_IGN_Vulcano"}).addTo(Teleport);
-L.marker([-169160.828125,-31371.564453125],{title:"MT_Teleporter_TAV_Converter"}).addTo(Teleport);
-L.marker([-154941.640625,29550.126953125],{title:"MT_Teleporter_TAV_DomeRuins"}).addTo(Teleport);
-L.marker([-215839.3125,48318.859375],{title:"MT_Teleporter_TAV_Duke"}).addTo(Teleport);
-L.marker([-195958.75,39933.5234375],{title:"MT_Teleporter_TAV_Fort"}).addTo(Teleport);
-L.marker([-246721.21875,-17115.876953125],{title:"MT_Teleporter_TAV_OUTIsland"}).addTo(Teleport);
-L.marker([-169045.96875,-95090.578125],{title:"MT_Teleporter_TAV_SandyPines"}).addTo(Teleport);
-L.marker([-243673.359375,46928.10546875],{title:"MT_Teleporter_TAV_SouthCliffs"}).addTo(Teleport);
-L.marker([-120211.4453125,-58722.24609375],{title:"MT_Teleporter_TAV_TavarMountain"}).addTo(Teleport);
-L.marker([-227834.5,10642.90625],{title:"MT_Teleporter_TAV_WindPark"}).addTo(Teleport);
-L.marker([62177.9296875,-125421.71875],{title:"MT_Teleporter_XAC_ConverterEast"}).addTo(Teleport);
-L.marker([22325.5703125,-142126.703125],{title:"MT_Teleporter_XAC_ConverterWest"}).addTo(Teleport);
-L.marker([74059.34375,-75361.515625],{title:"MT_Teleporter_XAC_EntranceEast"}).addTo(Teleport);
-L.marker([13854.291015625,-122864.4375],{title:"MT_Teleporter_XAC_EntranceSouth"}).addTo(Teleport);
-L.marker([81886.0703125,-161741.984375],{title:"MT_Teleporter_XAC_Icepalace"}).addTo(Teleport);
-}
+var arr = [
+[ -196259.625, 40234.38671875, 31708.568359375, "MapMarker_Location_TAV_Fort", "MT_Teleporter_TAV_Fort" ],
+[ -155198.21875, 29806.705078125, 29256.07421875, "MapMarker_Location_TAV_DomeRuins", "MT_Teleporter_TAV_DomeRuins" ],
+[ -228052.921875, 10861.32421875, 28500.705078125, "MapMarker_Location_TAV_WindPark", "MT_Teleporter_TAV_WindPark" ],
+[ -243922.640625, 47177.390625, 28523.1328125, "MapMarker_Location_TAV_SouthCliffs", "MT_Teleporter_TAV_SouthCliffs" ],
+[ -169421.484375, -31118.638671875, 28742.17578125, "MapMarker_Location_TAV_Converter", "MT_Teleporter_TAV_Converter" ],
+[ -120468.0546875, -58465.63671875, 39306.39453125, "MapMarker_Location_TAV_TavarMountain", "MT_Teleporter_TAV_TavarMountain" ],
+[ -206926.515625, -156211.890625, 28683.14453125, "MapMarker_Location_EDA_FirstBase", "MT_Teleporter_EDA_FirstBase" ],
+[ -218261.34375, -133308.34375, 34146.19921875, "MapMarker_Location_EDA_Goliet", "MT_Teleporter_EDA_Goliet" ],
+[ -183055.8125, -162907.5625, 28579.8125, "MapMarker_Location_EDA_Converter", "MT_Teleporter_EDA_Converter" ],
+[ -238521.875, -113725.4375, 41812.875, "MapMarker_Location_EDA_HotelGoliet", "MT_Teleporter_EDA_HotelGoliet" ],
+[ -236145.640625, -173125.5625, 28271.361328125, "MapMarker_Location_EDA_WoodElevator", "MT_Teleporter_EDA_WoodElevator" ],
+[ -279004.65625, -222072.71875, 20913.4140625, "MapMarker_Location_EDA_PilgerLocation", "MT_Teleporter_EDA_PilgerLocation" ],
+[ -241704.734375, -231423.15625, 24155.4765625, "MapMarker_Location_EDA_CompanyPlace", "MT_Teleporter_EDA_CompanyPlace" ],
+[ -270851.71875, -131106.328125, 28330.611328125, "MapMarker_Location_EDA_RiverDelta", "MT_Teleporter_EDA_RiverDelta" ],
+[ -102076.2890625, -128870.9375, 50983.9140625, "MapMarker_Location_HOM_HomeBase", "MT_Teleporter_HOM_HomeBase" ],
+[ -216042.8125, -58350.6796875, 41305.890625, "MapMarker_Location_EDA_MountainLake", "MT_Teleporter_EDA_MountainLake" ],
+[ -84269.6328125, -179600.546875, 42254.0390625, "MapMarker_Location_ABE_Relict", "MT_Teleporter_ABE_Relict" ],
+[ -30914.361328125, -133614.3125, 47093.35546875, "MapMarker_Location_ABE_ConverterNorth", "MT_Teleporter_ABE_ConverterNorth" ],
+[ -60958.76171875, -84607.1015625, 46319.35546875, "MapMarker_Location_ABE_ConverterSouth", "MT_Teleporter_ABE_ConverterSouth" ],
+[ -36157.61328125, -53677.96875, 50177.60546875, "MapMarker_Location_ABE_Barrage", "MT_Teleporter_ABE_Barrage" ],
+[ -29914.765625, 95499.375, 49265.26171875, "MapMarker_Location_IGN_Crater", "MT_Teleporter_IGN_Crater" ],
+[ 28200.494140625, 117075.078125, 51546.328125, "MapMarker_Location_IGN_Hort", "MT_Teleporter_IGN_Hort" ],
+[ 49601.96875, -5547.6625976562, 54155.67578125, "MapMarker_Location_IGN_Converter", "MT_Teleporter_IGN_Converter" ],
+[ -85813.7890625, 112325.859375, 47182.23828125, "MapMarker_Location_IGN_LavaLake", "MT_Teleporter_IGN_LavaLake" ],
+[ -11880.717773438, 52323.4765625, 52657.80859375, "MapMarker_Location_IGN_Vulcano", "MT_Teleporter_IGN_Vulcano" ],
+[ 52299.5625, 84911.2734375, 55023.75390625, "MapMarker_Location_IGN_Hangar", "MT_Teleporter_IGN_Hangar" ],
+[ 13569.336914062, -122579.484375, 55579.2421875, "MapMarker_Location_XAC_EntranceSouth", "MT_Teleporter_XAC_EntranceSouth" ],
+[ 73755.5, -75057.671875, 59190.5546875, "MapMarker_Location_XAC_EntranceEast", "MT_Teleporter_XAC_EntranceEast" ],
+[ 61903.53515625, -125142.875, 58674.1484375, "MapMarker_Location_XAC_ConverterEast", "MT_Teleporter_XAC_ConverterEast" ],
+[ 22045.041015625, -141843.4375, 55415.44140625, "MapMarker_Location_XAC_ConverterWest", "MT_Teleporter_XAC_ConverterWest" ],
+[ 81586.0, -161441.921875, 61225.2421875, "MapMarker_Location_XAC_Icepalace", "MT_Teleporter_XAC_Icepalace" ],
+[ 11220.388671875, -62455.3828125, 56232.76171875, "MapMarker_Location_ABE_Zardom", "MT_Teleporter_ABE_Zardom" ],
+[ -54753.546875, 77432.8046875, 60519.64453125, "MapMarker_Location_IGN_ClericCamp", "MT_Teleporter_IGN_ClericCamp" ],
+[ 19096.904296875, 147144.78125, 52919.1875, "MapMarker_Location_IGN_Cathedral", "MT_Teleporter_IGN_Cathedral" ],
+[ -216081.234375, 48560.78125, 33762.3828125, "MapMarker_Location_TAV_Duke", "MT_Teleporter_TAV_Duke" ],
+[ -307896.84375, -183125.484375, 18183.544921875, "MapMarker_Location_EDA_BSKIsland", "MT_Teleporter_EDA_BSKIsland" ],
+[ -169285.328125, -94851.21875, 31356.322265625, "MapMarker_Location_TAV_SandyPines", "MT_Teleporter_TAV_SandyPines" ],
+[ -222114.609375, -107411.859375, 40334.16796875, "MapMarker_Location_EDA_GolietNorth", "MT_Teleporter_EDA_GolietNorth" ],
+[ -253455.359375, -205861.0625, 19090.564453125, "MapMarker_Location_EDA_ForbiddenValley", "MT_Teleporter_EDA_ForbiddenValley" ],
+[ -54772.625, 77488.1328125, 60130.8125, "Obj_Bld_Castle_Foundation_1", "none" ],
+[ -59262.3203125, -157100.59375, 45232.6796875, "MapMarker_Location_ABE_CentralAbessa", "MT_Teleporter_ABE_CentralAbessa" ],
+[ -103128.5546875, -200294.734375, 37746.50390625, "MapMarker_Location_ABE_SmallFarm", "MT_Teleporter_ABE_SmallFarm" ],
+[ -246943.03125, -16894.05859375, 18991.9140625, "MapMarker_Location_TAV_OUTIsland", "MT_Teleporter_TAV_OUTIsland" ],
+];
+var arr_len = 43;
+
+function add_teleport_markers() {
+    for (var i = 0; i < arr_len; i++) {
+        var m = arr[i];
+        L.marker( [ m[0]*0.01, m[1]*0.01 ], { title: m[3] } ).addTo(Teleport);
+    };    
+};
