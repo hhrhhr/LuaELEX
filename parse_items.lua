72ebc538b8268107a001fa5d8dec25d13a567f7e
Dmitry Zaitsev <hhrhhr@gmail.com>
2017-10-29 01:17:05 +0300

    update parsers for multi-language support

--- parse_items.lua
+++ parse_items.lua
@@ -1,16 +1,27 @@
 assert("Lua 5.3" == _VERSION)
-assert(arg[1], "\n\n[ERROR] no input file\n\n")
+assert(arg[1], "\n\n[ERROR] no input file\n")
+assert(arg[#arg], "\n\n[ERROR] no language file\n")
+
+local LANG = dofile(arg[#arg])
 
 --[[
 lua gar5_parser.lua *_Items.sec
     -> *.sec.lua
     
-lua parse_items.lua item1.lua [item2.lua[ ...]] > items.txt
+lua parse_items.lua item1.lua [item2.lua[ ...] ] > items.txt
 --]]
 
 local items = {}
 
-for i = 1, #arg do
+local function hash(str)
+    local hash = 5381
+    for i = 1, #str do
+        hash = (hash * 33 + str:byte(i)) & 0xffffffff
+    end
+    return hash
+end
+
+for i = 1, #arg-1 do
     dofile(arg[i])
 
     local t = string.gsub(arg[i], "(.-)([^\\]-[^\\%.]+).sec.lua$", "%2")
@@ -30,13 +41,28 @@ for i = 1, #arg do
         local pos = cD.position
         local id = cE.string1
 
-        if id:find("It_Audiolog_") == 1
+        if id:find("It_Am_") == 1
+        or id:find("It_Audiolog_") == 1
+--        or id:find("It_Blaster_") == 1
+--        or id:find("It_Bow_") == 1
+--        or id:find("It_Crossbow_") == 1
+--        or id:find("It_Flamethrower_") == 1
+--        or id:find("It_GrenadeLaucher_") == 1
         or id:find("It_Pic_") == 1
         or id:find("It_Recipe_") == 1
         or id:find("It_SocketItem_") == 1
         or id:find("It_Wri_") == 1
+--        if id:find("It_1h_") == 1
+--        or id:find("It_2h_") == 1
         then
             table.insert(pos, "\""..id.."\"")
+
+            local fo = hash(("FO_" .. id):lower())
+            local desc = hash(("ITEMDESC_" .. id):lower())
+            
+            table.insert(pos, string.format("%q", LANG[fo]))
+            table.insert(pos, string.format("%q", LANG[desc]))
+
             table.insert(items, pos)
         end
     end
@@ -45,5 +71,5 @@ end
 table.sort(items, function(a, b) return a[4] < b[4] end)
 
 for i = 1, #items do
-    print(table.concat(items[i], ", "))
+    print("[ " .. table.concat(items[i], ", ") .. " ],")
 end
--- parse_teleports.lua
+++ parse_teleports.lua
@@ -1,5 +1,8 @@
 assert("Lua 5.3" == _VERSION)
 assert(arg[1], "\n\n[ERROR] no input file\n\n")
+assert(arg[#arg], "\n\n[ERROR] no language file\n")
+
+local LANG = dofile(arg[#arg])
 
 --[[
 lua gar5_parser.lua World_Teleporter.sec
@@ -11,12 +14,20 @@ lua parse_teleports.lua World_Teleporter.sec.lua > teleport.js
 
 dofile(arg[1])
 
+local function hash(str)
+    local hash = 5381
+    for i = 1, #str do
+        hash = (hash * 33 + str:byte(i)) & 0xffffffff
+    end
+    return hash
+end
+
 local w = World_Teleporter
 
 local data = w["class gCEmbeddedLayer"]
-    .data["class gCEmbeddedLayer"]["gEEntityType_Game"]["class eCScene"]
-    .data["class eCScene"]["class eCDynamicEntity"]
-    .data["class eCEntity"]
+.data["class gCEmbeddedLayer"]["gEEntityType_Game"]["class eCScene"]
+.data["class eCScene"]["class eCDynamicEntity"]
+.data["class eCEntity"].eCEntity2
 
 io.write("var arr = [\n")
 for i = 1, #data do
@@ -24,14 +35,15 @@ for i = 1, #data do
     local cD = d["class eCDynamicEntity"]
     local cE = d["class eCEntity"]
 
-    local l = cE[3] and cE[3]["class gCMapLocation_PS"].prop
+    local l = cE.eCEntity1[3] and cE.eCEntity1[3]["class gCMapLocation_PS"].prop
 
     local pos = cD.position
     local id = cE.string1
     local title = l and l["Title"] or "none"
-
+    title = hash(title:lower())
+    
     table.insert(pos, "\"" .. id .. "\"")
-    table.insert(pos, "\"" .. title .. "\"")
+    table.insert(pos, string.format("%q", LANG[title] or "---"))
 
     io.write("[ ")
     io.write(table.concat(pos, ", "))
@@ -43,7 +55,8 @@ io.write([[
 function add_teleport_markers() {
     for (var i = 0; i < arr_len; i++) {
         var m = arr[i];
-        L.marker( [ m[0], m[1] ], { title: m[3] } ).addTo(Teleport);
+        var pop = m[4] + "<br /><i>" + m[3] + "</i>";
+        L.marker( [ m[0]*0.01, m[1]*0.01 ], { title: m[4], icon: teleport } ).bindPopup(pop).addTo(Teleport);
     };    
 };
 
