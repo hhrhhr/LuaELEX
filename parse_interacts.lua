1542c6db720cd5204275d94888311a4c66682981
Dmitry Zaitsev <hhrhhr@gmail.com>
2017-11-10 11:07:58 +0300

    update parsers with new style

---
+++ parse_interacts.lua
@@ -0,0 +1,130 @@
+assert("Lua 5.3" == _VERSION)
+local args = #arg
+assert(arg[1], "\n\n[ERROR] no input file\n")
+assert(arg[args-1], "\n\n[ERROR] no language file\n")
+assert(arg[args], "\n\n[ERROR] no output file\n")
+
+local LANG = dofile(arg[args-1])
+local OUT = assert(io.open(arg[args], "w+"))
+
+local owr = function(fmt, ...)
+    local str = string.format(fmt, ...)
+    OUT:write(str)
+end
+
+local COFF = 0.01
+
+--[[
+lua gar5_parser.lua *_Interacts.sec
+    -> *.sec.lua
+    
+lua parse_interacts.lua <int1.lua> [int2.lua [...] ] <lang.lua> <output.js>
+--]]
+
+local orevein = {}
+local used_lang = {}
+
+local function hash(str)
+    local hash = 5381
+    for i = 1, #str do
+        hash = (hash * 33 + str:byte(i)) & 0xffffffff
+    end
+    used_lang[hash] = true
+    return hash
+end
+
+for i = 1, args-2 do
+    dofile(arg[i])
+
+    local class = string.gsub(arg[i], "(.-)([^\\]-[^\\%.]+).sec.lua$", "%2")
+    io.stderr:write(class .. "\n")
+    local w = _G[class]
+
+    local data = w["class gCEmbeddedLayer"]
+    .data["class gCEmbeddedLayer"]["gEEntityType_Game"]["class eCScene"]
+    .data["class eCScene"]["class eCDynamicEntity"]
+    .data["class eCEntity"].eCEntity2
+
+    for j = 1, #data do
+        local d = data[j]["class eCDynamicEntity"].data
+        local cD = d["class eCDynamicEntity"]
+        local cE = d["class eCEntity"]
+
+        local pos = cD.bb_mid
+        local id = cE.string1
+
+        if id:find("Obj_Int_") == 1 then
+            if id:find("OreVein_", 9) == 9 then
+                local ore
+                if id:find("Elex", 17) == 17 then
+                    ore = "elex"
+                elseif id:find("Gold", 17) == 17 then
+                    ore = "gold"
+                elseif id:find("Iron", 17) == 17 then
+                    ore = "iron"
+                elseif id:find("Sulfur", 17) == 17 then
+                    ore = "sulfur"
+                elseif id:find("Depleted", 17) == 17 then
+                    ore = "empty"
+                end
+                if ore then
+                    local t = {}
+                    t[1] = ("%.2f"):format(pos[1] * COFF)
+                    t[2] = ("%.2f"):format(pos[3] * COFF)
+                    t[3] = ("\"%s#%d %s\""):format(class, j, id)
+                    local fo = hash(("FO_" .. id):lower())
+                    t[4] = ("0x%08X"):format(fo)
+                    t[5] = ("\"%s\""):format(ore)
+
+                    table.insert(orevein, t)
+                end
+            end
+        end
+    end
+end
+
+table.sort(orevein, function(a, b) return a[3] < b[3] end)
+
+local lang = {}
+for k, _ in pairs(used_lang) do
+    local str = LANG[k]
+    if not str then str = "---" end
+    table.insert(lang, {k, str})
+end
+used_lang = nil
+table.sort(lang, function(a, b) return a[1] < b[1] end)
+
+
+-- lang array
+owr("var lang = [];\n")
+for i = 1, #lang do
+    local l = lang[i]
+    owr("lang[0x%08X] = %q;\n", l[1], l[2])
+end
+owr("\n")
+
+-- interacts array
+owr("var arr_ore = [\n")
+for i = 1, #orevein do
+    owr("[ %s ],\n", table.concat(orevein[i], ", "))
+end
+owr("];\n")
+
+-- funcs
+owr([=[
+
+init_marker.push(
+  function () {
+    for (var i = 0; i < arr_ore.length; i++) {
+      var m = arr_ore[i];
+      var id = lang[m[3]];
+      var pop = "<b>" + id + "</b><br /><i>" + m[2] + "</i>";
+      L.marker( [ m[1], m[0] ], { title: id, icon: icon[m[4]] } )
+      .bindPopup(pop).addTo(Orevein);
+    };
+    arr_ore = null;
+  }
+);
+]=])
+
+OUT:close()
--- parse_items.lua
+++ parse_items.lua
@@ -1,10 +1,16 @@
 assert("Lua 5.3" == _VERSION)
+local args = #arg
 assert(arg[1], "\n\n[ERROR] no input file\n")
-assert(arg[#arg-1], "\n\n[ERROR] no language file\n")
-assert(arg[#arg], "\n\n[ERROR] no output file\n")
+assert(arg[args-1], "\n\n[ERROR] no language file\n")
+assert(arg[args], "\n\n[ERROR] no output file\n")
 
-local LANG = dofile(arg[#arg-1])
-local OUT = assert(io.open(arg[#arg], "w+"))
+local LANG = dofile(arg[args-1])
+local OUT = assert(io.open(arg[args], "w+"))
+
+local owr = function(fmt, ...)
+    local str = string.format(fmt, ...)
+    OUT:write(str)
+end
 
 local COFF = 0.01
 
@@ -12,7 +18,7 @@ local COFF = 0.01
 lua gar5_parser.lua *_Items.sec
     -> *.sec.lua
     
-lua parse_items.lua <item1.lua> [item2.lua [...] ] <lang.lua> <output.txt>
+lua parse_items.lua <item1.lua> [item2.lua [...] ] <lang.lua> <output.js>
 --]]
 
 local items = {}
@@ -27,56 +33,60 @@ local function hash(str)
     return hash
 end
 
-for i = 1, #arg-2 do
+for i = 1, args-2 do
     dofile(arg[i])
 
-    local t = string.gsub(arg[i], "(.-)([^\\]-[^\\%.]+).sec.lua$", "%2")
-    io.stderr:write(t .. "\n")
-    local w = _G[t]
+    local class = string.gsub(arg[i], "(.-)([^\\]-[^\\%.]+).sec.lua$", "%2")
+    io.stderr:write(class .. "\n")
+    local w = _G[class]
 
     local data = w["class gCEmbeddedLayer"]
     .data["class gCEmbeddedLayer"]["gEEntityType_Game"]["class eCScene"]
     .data["class eCScene"]["class eCDynamicEntity"]
     .data["class eCEntity"].eCEntity2
 
-    for i = 1, #data do
-        local d = data[i]["class eCDynamicEntity"].data
+    for j = 1, #data do
+        local d = data[j]["class eCDynamicEntity"].data
         local cD = d["class eCDynamicEntity"]
         local cE = d["class eCEntity"]
 
         local pos = cD.bb_mid
-        table.remove(pos, 2)
-        pos[1] = pos[1] * COFF
-        pos[2] = pos[2] * COFF
-
         local id = cE.string1
 
         if id:find("It_") == 1 then
-            if id:find("Am_", 4) == 4
-            or id:find("Audiolog_", 4) == 4
---            or id:find("It_Blaster_", 4) == 4
---            or id:find("It_Bow_", 4) == 4
---            or id:find("It_Crossbow_", 4) == 4
---            or id:find("It_Flamethrower_", 4) == 4
---            or id:find("It_GrenadeLaucher_", 4) == 4
-            or id:find("Pic_", 4) == 4
-            or id:find("Recipe", 4) == 4
-            or id:find("Ri_", 4) == 4
-            or id:find("SocketItem_", 4) == 4
-            or id:find("Sun_", 4) == 4
-            or id:find("Wri_Book", 4) == 4
-            or id:find("Wri_Letter", 4) == 4
---            or id:find("It_1h_", 4) == 4
---            or id:find("It_2h_", 4) == 4
-            then
-                table.insert(pos, "\""..id.."\"")
-
+            local item
+            if id:find("Am_", 4) == 4 then
+                item = "amulet"
+            elseif id:find("Audiolog_", 4) == 4 then
+                item = "audio"
+            elseif id:find("Pic_", 4) == 4 then
+                item = "picture"
+            elseif id:find("Recipe", 4) == 4 then
+                item = "recipe"
+            elseif id:find("Ri_", 4) == 4 then
+                item = "amulet"
+            elseif id:find("SocketItem_", 4) == 4 then
+                item = "socket"
+            elseif id:find("Sun_", 4) == 4 then
+                item = "sunglass"
+            elseif id:find("Wri_Book", 4) == 4 then
+                item = "book"
+            elseif id:find("Wri_Letter", 4) == 4 then
+                item = "letter"
+            end
+            
+            if item then
+                local t = {}
+                t[1] = ("%.2f"):format(pos[1] * COFF)
+                t[2] = ("%.2f"):format(pos[3] * COFF)
+                t[3] = ("\"%s#%d %s\""):format(class, j, id)
                 local fo = hash(("FO_" .. id):lower())
                 local desc = hash(("ITEMDESC_" .. id):lower())
-                table.insert(pos, ("0x%08X"):format(fo))
-                table.insert(pos, ("0x%08X"):format(desc))
+                t[4] = ("0x%08X"):format(fo)
+                t[5] = ("0x%08X"):format(desc)
+                t[6] = ("\"%s\""):format(item)
 
-                table.insert(items, pos)
+                table.insert(items, t)
             end
         end
     end
@@ -85,38 +95,46 @@ end
 table.sort(items, function(a, b) return a[3] < b[3] end)
 
 local lang = {}
-for k, v in pairs(used_lang) do
-    table.insert(lang, {k, LANG[k]})
+for k, _ in pairs(used_lang) do
+    local str = LANG[k]
+    if not str then str = "---" end
+    table.insert(lang, {k, str})
 end
 used_lang = nil
 table.sort(lang, function(a, b) return a[1] < b[1] end)
 
+
 -- lang array
-OUT:write("var lang = [];\n")
+owr("var lang = [];\n")
 for i = 1, #lang do
     local l = lang[i]
-    OUT:write(string.format("lang[0x%08X] = %q;\n", l[1], l[2]))
+    owr("lang[0x%08X] = %q;\n", l[1], l[2])
 end
-OUT:write("\n")
+owr("\n")
 
 -- items array
+owr("var arr_item = [\n")
 for i = 1, #items do
-    OUT:write("[ " .. table.concat(items[i], ", ") .. " ],\n")
+    owr("[ %s ],\n", table.concat(items[i], ", "))
 end
-OUT:write("\n")
+owr("];\n")
 
 -- funcs
-OUT:write("function add_markers() {\n")
+owr([=[
+    
+init_marker.push(
+  function () {
+    for (var i = 0; i < arr_item.length; i++) {
+      var m = arr_item[i];
+      var id = lang[m[3]];
+      var desc = lang[m[4]];
+      var pop = "<b>" + id + "</b><br />" + desc + "<br /><i>" + m[2] + "</i>"
+      L.marker( [ m[1], m[0] ], { title: id, icon: icon[m[5]] } )
+      .bindPopup(pop).addTo(layer[m[5]]);
+    };
+    arr_item = null;
+  }
+);
+]=])
 
-local q = { {"am", "Amulet"}, {"audio", "Audio"}, {"pic", "Picture"}, {"recipe", "Recipe"},
-    {"socket", "Socket"}, {"sun", "Sunglasses"}, {"book", "Book"}, {"letter", "Letter"} }
-for i = 1, #q do
-    OUT:write("    for (var i = 0; i < arr_"..q[i][1]..".length; i++) {\n")
-    OUT:write("        var m = arr_"..q[i][1].."[i];\n")
-    OUT:write("        var pop = \"<b>\" + lang[m[3]] + \"</b><br />\" + lang[m[4]] + \"<br /><i>\" + m[2] + \"</i>\"\n")
-    OUT:write("        L.marker( [ m[1], m[0] ], { title: lang[m[3]], icon: "..q[i][2]:lower().." } )\n")
-    OUT:write("        .bindPopup(pop).addTo("..q[i][2]..");\n    };\n")
-end
-
-OUT:write("};\n")
 OUT:close()
